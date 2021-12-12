94b5a5b1b0b7ab18cdc088694411af209a2a0adc
Alexander <58190800+Alexander-pixel@users.noreply.github.com>
2021-12-12 19:52:50 +0200

    Update 1-pipe.js

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => x => {
+  if (fns.length === 0) return x;
+  let res = x;
+  for (const fn of fns) {
+    if (typeof fn !== 'function') throw new Error('Wrong parameter.');
+    res = fn(res);
+  }
+  return res;
+};
 
 module.exports = { pipe };
